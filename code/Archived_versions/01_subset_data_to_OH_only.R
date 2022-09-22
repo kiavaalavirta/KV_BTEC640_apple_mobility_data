202b354021c6b9c797eba75eee5db7c35617e645
Kia Vaalavirta <kavaalavirta@dons.usfca.edu>
2022-09-21 18:02:14 -0700

    Add code files for step one

---
+++ code/Archived_versions/01_subset_data_to_OH_only.R
@@ -0,0 +1,17 @@
+# Script to read in Apple mobility data from a csv file
+# and subset the data to just include rows from Ohio
+# and then write out a cvs file that only has that subset.
+
+# Kia Vaalavirta
+# kavaalavirta@dons.usfca.edu
+# September 2, 2022
+
+# read in the complete csv file 
+all_covid_data <- read.csv("data/raw data/applemobilitytrends-2022-04-12.csv")
+
+# subset the dataset to only include rows where the sub.region column has
+# "Ohio" in it but we want all columns.
+ohio_data <- all_covid_data[all_covid_data$sub.region == "Ohio", ]
+
+# save the Ohio data to a new csv file in the output directory.
+write.csv(ohio_data, file = "output/ohio_apple_mobility_data.csv")
---
+++ code/Functions/subset_to_a_state.R
@@ -0,0 +1,32 @@
+# Script to read in Apple mobility data from a csv file
+# and subset the data to just include rows from any particular US state
+# and then write out a cvs file that only has that subset.
+
+# Kia Vaalavirta
+# kavaalavirta@dons.usfca.edu
+# September 17, 2022
+
+# create a function to subset any US state out of the full data set
+# by also creating an output CSV file named based on the state
+subset_to_a_state <- function(input_file_name,
+                              state_name) {
+  # read in the complete csv file 
+  all_covid_data <- read.csv(input_file_name)
+
+  # subset the dataset just to the state of our choosing
+  state_dataset <- all_covid_data[all_covid_data$sub.region == state_name, ]
+  
+
+  # check that the subsetted data actually has data in it
+  if (nrow(state_dataset) == 0)  { 
+    stop("ERROR: No rows matching given state name. Did you make a typo?")
+} 
+    
+  # save the subsetted state data to a new csv file in the output directory
+  write.csv(state_dataset, file = paste0("output/",
+                                        tools::file_path_sans_ext(
+                                          basename(input_file_name)),
+                                        "_",
+                                        state_name,
+                                        ".csv"))
+}
