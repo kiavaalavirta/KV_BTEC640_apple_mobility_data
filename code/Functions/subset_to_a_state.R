884a001f153e2c8d11949dfa6ba562e0f6d6012a
Kia Vaalavirta <kavaalavirta@dons.usfca.edu>
2022-09-26 14:14:42 -0700

    Edit subset state fuction file to pass lintr check

--- code/Functions/subset_to_a_state.R
+++ code/Functions/subset_to_a_state.R
@@ -10,18 +10,18 @@
 # by also creating an output CSV file named based on the state
 subset_to_a_state <- function(input_file_name,
                               state_name) {
-  # read in the complete csv file 
+  # read in the complete csv file
   all_covid_data <- read.csv(input_file_name)
 
   # subset the dataset just to the state of our choosing
   state_dataset <- all_covid_data[all_covid_data$sub.region == state_name, ]
-  
+
 
   # check that the subsetted data actually has data in it
-  if (nrow(state_dataset) == 0)  { 
+  if (nrow(state_dataset) == 0)  {
     stop("ERROR: No rows matching given state name. Did you make a typo?")
-} 
-    
+}
+
   # save the subsetted state data to a new csv file in the output directory
   write.csv(state_dataset, file = paste0("output/",
                                         tools::file_path_sans_ext(
