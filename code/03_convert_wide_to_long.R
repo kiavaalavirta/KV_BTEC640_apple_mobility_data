6f0a360c74f84028278a738ca3523ce48495d125
Kia Vaalavirta <kavaalavirta@dons.usfca.edu>
2022-10-09 17:42:25 -0700

    Test wide to long function

---
+++ code/03_convert_wide_to_long.R
@@ -0,0 +1,11 @@
+# Kia Vaalavirta
+# October 9, 2022
+# kavaalavirta@dons.usfca.edu
+
+# load functions
+source("code/functions/subset_to_a_state.R")
+
+# test out the use of the function
+convert_wide_to_long(
+  file_name_in =
+    "output/subsetted_states_wide/applemobilitytrends-2022-04-12_Alabama.csv")
