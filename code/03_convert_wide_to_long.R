8c6675735083384bb6710b71b2688357f67bcdeb
kiavaalavirta <112041614+kiavaalavirta@users.noreply.github.com>
2022-10-15 22:33:00 -0700

    Add description for wide to long script

--- code/03_convert_wide_to_long.R
+++ code/03_convert_wide_to_long.R
@@ -1,3 +1,8 @@
+# the goal of this script is to change subsetted data of any state
+# that has Apple mobility data available from wide to long
+# and display relative mobility in the new csv file that is saved
+# in output/subsetted_states_long directory
+
 # Kia Vaalavirta
 # October 9, 2022
 # kavaalavirta@dons.usfca.edu
@@ -5,7 +10,7 @@
 # load functions
 source("code/functions/subset_to_a_state.R")
 
-# test out the use of the function
+# run the function for given state
 convert_wide_to_long(
   file_name_in =
     "output/subsetted_states_wide/applemobilitytrends-2022-04-12_Alabama.csv")
