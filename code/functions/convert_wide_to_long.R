3640a1abe940ecf3f248fd92a9b19491bfa79674
kiavaalavirta <112041614+kiavaalavirta@users.noreply.github.com>
2022-10-15 22:16:26 -0700

    Edit wide to long function

--- code/functions/convert_wide_to_long.R
+++ code/functions/convert_wide_to_long.R
@@ -2,28 +2,29 @@
 # October 9, 2022
 # kavaalavirta@dons.usfca.edu
 
-# create a function to change the data from wide to long
-convert_wide_to_long <- function(file_name_in) {
-  library("tidyr")
-  library("readr")
-  library("dplyr")
+# load needed packages
+library("tidyr")
+library("readr")
+library("dplyr")
 
+# create a function to change the data from wide to long for any state
+# by also creating an output CSV file named based on the state
+convert_wide_to_long <- function(file_name_in) {
+
+  # read in the complete csv file
   input_data <- readr::read_csv(file_name_in)
+
+  # change the data from wide to long for the specific state
   date_order <- input_data %>%
     pivot_longer(
       cols = starts_with("20"),
       names_to = "date",
       values_to = "rel_mobility")
 
-readr::write_csv(date_order, file = paste0("output/subsetted_states_long/",
-                                           tools::file_path_sans_ext(
-                                             basename(file_name_in)),
-                                           "_long",
-                                           ".csv"))
+  # save the data to a new csv file in the "subsetted_states_long" directory.
+  readr::write_csv(date_order, file = paste0("output/subsetted_states_long/",
+                                             tools::file_path_sans_ext(
+                                               basename(file_name_in)),
+                                             "_long",
+                                             ".csv"))
 }
-
-
-# test out the use of the function
-convert_wide_to_long(
-  file_name_in =
-    "output/subsetted_states_wide/applemobilitytrends-2022-04-12_Alabama.csv")
