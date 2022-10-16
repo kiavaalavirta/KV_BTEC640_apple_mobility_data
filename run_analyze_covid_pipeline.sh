ad84e27a8b2ca9080ab8515978f438bd27ae4920
kiavaalavirta <112041614+kiavaalavirta@users.noreply.github.com>
2022-10-16 16:42:10 -0700

    Fix bash render script

--- run_analyze_covid_pipeline.sh
+++ run_analyze_covid_pipeline.sh
@@ -3,4 +3,4 @@
 # A bash script to drive the rendering of an Rmarkdown file using some
 # parameters passed in from the shell
 
-Rscript -e "rmarkdown::render("analysis.Rmd")
+Rscript -e "rmarkdown::render("analysis.Rmd")"
