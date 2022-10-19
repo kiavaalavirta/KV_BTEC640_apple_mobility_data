eda7b2970efd200d42be0f53393dac5b5c1d82fb
kiavaalavirta <112041614+kiavaalavirta@users.noreply.github.com>
2022-10-18 21:43:33 -0700

    Add if statement to check for parameters

--- run_analyze_covid_pipeline.sh
+++ run_analyze_covid_pipeline.sh
@@ -1,4 +1,4 @@
-#|/bin/bash
+#!/bin/bash
 
 # A bash script to drive the rendering of an Rmarkdown file using some
 # parameters passed in from the shell
@@ -6,9 +6,17 @@
 # the first should be a US state, and the second is the raw apple mobility
 # data csv file downloaded from apple.com
 
-# Kia Vaalavirta
+# Kia Vaalavirta  
 # October 18, 2022
 # kavaalavirta@dons.usfca.edu
+
+if [ $# -eq 0 ]
+then 
+  echo "To run this script, supply two arguments:"
+  echo "The first is the name of the US state."
+  echo "The second is the path to the mobility data csv file"
+  exit 1
+fi
 
 Rscript -e "rmarkdown::render('analysis.Rmd',\
             params = list(state = '$1',\
