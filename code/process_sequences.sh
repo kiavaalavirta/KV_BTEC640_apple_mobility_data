5de5c70aad8b47b69ac9c2d3f06f02977ca43a99
Kia Vaalavirta <kavaalavirta@dons.usfca.edu>
2022-10-31 19:00:58 +0000

    Update bash script

--- code/process_sequences.sh
+++ code/process_sequences.sh
@@ -6,4 +6,34 @@
 # October 28, 2022
 # kavaalavirta@dons.usfca.edu
 
+# path to a single compressed fasta file as a command-line argument
+filepath=$1
+
+# check to make sure there is exactly one command-line argument
+if [ $# -ne 1 ]
+ then
+   echo "To run this script, supply one argument."
+   echo "The argument is the path to a compressed fasta file."
+   exit 1
+ fi
+
+# show the first 10 lines of the data to peek into its structure
+zcat $1 | head -n 10
+echo "First 10 lines of the data are displayed."
+
+#The first sequence found by its name
+zgrep ">NC_045512.2" $1
+echo "This is the first sequence."
+
+#show the first ten sequence names and comments
+bioawk -c fastx '{print $name $comment}' $1 | head -n 10
+echo "These are the first ten sequence names with comments."
+
+# return a count of the sequences in the file
+zgrep -oE "^>" $1 | wc -l
+echo "This is the number of sequences in the file."
+
+# return a count of sequences from China
+bioawk -c fastx '/China/ {print $name}' $1 | wc -l
+echo "This is the number of sequences from China."
 
