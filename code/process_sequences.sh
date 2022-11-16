91e8800ead6bfb3858821a4e91c583d5a1fdaafe
Kia Vaalavirta <kavaalavirta@dons.usfca.edu>
2022-11-16 05:27:30 +0000

    fix shellcheck issues and modify the script

--- code/process_sequences.sh
+++ code/process_sequences.sh
@@ -7,9 +7,10 @@
 # October 28, 2022
 # kavaalavirta@dons.usfca.edu
 
-# add code to make sure that all commands in  pipelines run
+# add code to make sure that all commands in pipelines run
 set -eou pipefail
 
+# give the path to the compressed fasta file as a command-line argument
 # check to make sure there is exactly one command-line argument
 if [ $# -ne 1 ]
  then
@@ -18,30 +19,12 @@ if [ $# -ne 1 ]
    exit 1
  fi
 
-# path to the compressed fasta file as a command-line argument
-path=$1
-
-# show the first 10 lines of the data to peek into its structure
-zcat "$1" | head -n 10
-echo "First 10 lines of the data are displayed."
-
-#The first sequence found by its name
-zgrep ">NC_045512.2" "$1"
-echo "This is the first sequence."
-
-#show the first ten sequence names and comments
-bioawk -c fastx '{print $name $comment}' "$1" | head -n 10
-echo "These are the first ten sequence names with comments."
-
 # return a count of the sequences in the file
 zgrep -oE "^>" "$1" | wc -l
 echo "This is the number of sequences in the file."
 
-# return a count of sequences from China
-bioawk -c fastx '/China/ {print $name}' "$1" | wc -l
-echo "This is the number of sequences from China."
-
 # tally up the number of SARS-CoV-2 sequences in the dataset from each country
 # and sort them from most to least sequences
 zgrep ">" "$1" | cut -d '|' -f 21 | sort | uniq -c | sort -r -k1 -n
-echo This is the number of sequences from each country.
+echo "This is the number of sequences from each country."
+echo "The countries are sorted from most to least sequences."
